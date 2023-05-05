2b42b715ad16d83d0400e96b58557962102dcf9e
hyrumbravo <118159809+hyrumbravo@users.noreply.github.com>
2023-05-06 00:09:41 +0800

    edited profile yana

---
+++ app/src/main/java/com/example/tffifeeder/profile_yana.java
@@ -0,0 +1,14 @@
+package com.example.tffifeeder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile_yana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_yana);
+    }
+}
