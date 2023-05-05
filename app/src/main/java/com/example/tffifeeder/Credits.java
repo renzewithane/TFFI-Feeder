3195f695e64b53b2472982e71f647164ee85f485
Renze Meinard Mortiga <98935669+renzewithane@users.noreply.github.com>
2023-05-05 23:30:58 +0800

    Fixed App

---
+++ app/src/main/java/com/example/tffifeeder/Credits.java
@@ -0,0 +1,14 @@
+package com.example.tffifeeder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
