7b5f5622de4b6e99fde72fcee29c20ba4a82fc78
Renze Meinard Mortiga <98935669+renzewithane@users.noreply.github.com>
2023-05-05 21:29:22 +0800

    Updated Splash Screen and Fixed Dashboard

--- app/src/main/java/com/example/tffifeeder/Home.java
+++ app/src/main/java/com/example/tffifeeder/Home.java
@@ -2,7 +2,10 @@ package com.example.tffifeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        LinearLayout linearLayout = findViewById(R.id.acc_button);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, PetInfo.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/tffifeeder/PetInfo.java
@@ -0,0 +1,14 @@
+package com.example.tffifeeder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_info);
+    }
+}
