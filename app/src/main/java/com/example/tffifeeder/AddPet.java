a22795cb9294b29da18ba98e97407a08b6e3e6e2
Arriana Mae Malubay <arimamalubay333@gmail.com>
2023-05-06 16:32:34 +0800

    Change Navigation Layout and Fixed Errors

--- app/src/main/java/com/example/tffifeeder/AddPet.java
+++ app/src/main/java/com/example/tffifeeder/AddPet.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class add_pet extends AppCompatActivity {
+public class AddPet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
